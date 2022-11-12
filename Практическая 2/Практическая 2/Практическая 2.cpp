c8386c48a996f86e81a1b08e595b67e8b11bdedc
User <User@DESKTOP-IBLNQ92>
2022-11-12 17:22:36 +0300

    practice 3

---
+++ Практическая 2/Практическая 2/Практическая 2.cpp
@@ -0,0 +1,358 @@
+﻿#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+using namespace chrono;
+
+void random(int Arr[], const int n) {
+    for (int i = 0; i < n; i++) {
+        Arr[i] = rand() % 199 - 99;
+    }
+}
+
+void print_arr(int Arr[], const int n) {
+    for (int i = 0; i < n; i++) {
+        cout << Arr[i] << " ";
+    }
+}
+
+void bubble_sort(int Arr[], const int n)
+{
+    for (int j = 0; j < (n - 1); j++) {
+        for (int i = 0; i < (n - 1 - j); i++) {
+            if (Arr[i] > Arr[i + 1]) {
+                swap(Arr[i], Arr[i + 1]);
+            }
+        }
+    }
+}
+
+void shaker_sort(int Arr[], const int n)
+{
+    bool flag = true;
+    int right = n - 1;
+    int left = 1;
+    while (flag) {
+        flag = false;
+        for (int i = left; i <= right; i++) {
+            if (Arr[i - 1] > Arr[i]) {
+                swap(Arr[i - 1], Arr[i]);
+                flag = true;
+            }
+        }
+        right--;
+        for (int i = right; i >= left; i--) {
+            if (Arr[i] < Arr[i - 1]) {
+                swap(Arr[i], Arr[i - 1]);
+                flag = true;
+            }
+        }
+        left++;
+    }
+}
+
+void comb_sort(int Arr[], const int n)
+{
+    float k = 1.247;
+    float S = n - 1;
+    while (S >= 1) {
+        for (int i = 0; i + S < n; i++) {
+            if (Arr[i] > Arr[int(i + S)]) {
+                swap(Arr[int(i + S)], Arr[i]);
+            }
+        }
+        S /= k;
+    }
+
+    int count = 0;
+    while (true) {
+        for (int i = 0; i < n - 1; i++) {
+            if (Arr[i] > Arr[i + 1]) {
+                swap(Arr[int(i + S)], Arr[i]);
+            }
+            else count++;
+        }
+        if (count == n - 1)
+            break;
+        else
+            count = 0;
+    }
+}
+
+void insert_sort(int Arr[], const int n)
+{
+    for (int j = 0; j < n - 1; j++) {
+        if (Arr[j] > Arr[j + 1]) {
+            for (int i = j; i >= 0; i--) {
+                if (Arr[i] > Arr[i + 1]) {
+                    swap(Arr[i], Arr[i + 1]);
+                }
+            }
+        }
+    }
+}
+
+void quick_sort(int Arr[], int  end, int start)
+{
+    int mid, s = start, e = end;
+    mid = Arr[(s + e) / 2];
+    while (s < e)
+    {
+        while (Arr[s] < mid) s++;
+        while (Arr[e] > mid) e--;
+        if (s <= e)
+        {
+            swap(Arr[s], Arr[e]);
+            s++;
+            e--;
+        }
+    }
+    if (start < e) quick_sort(Arr, e, start);
+    if (s < end) quick_sort(Arr, end, s);
+}
+
+int main()
+{
+    setlocale(0, "Rus");
+    srand(time(0));
+    const int N = 100;
+    int Array[N], SortArray[N];
+    cout << "1. Создать новый несортированный массив\n2. Отсортировать массив\n3. Найти максимальный и минимальный элемент\n4. Нaйти среднее значение и индексы элементов, равных ему\n5. Найти количество элементов, которые меньше числа a\n6. Найти количество элеметнов, которые больше числа b\n7. Найти элемент в массиве\n8. Поменять местами элементы a и b массива\n0. Завершить работу программы\n\n";
+    for (int i = 0; i < N; i++) {
+        Array[i] = rand() % 199 - 99;
+        SortArray[i] = Array[i];
+    }
+    cout << "Текущий неотсортированный массив:\n";
+    print_arr(Array, N);
+    cout << "\n\n";
+
+    while (true) {
+        int ind;
+        cin >> ind;
+        if (ind == 1) {
+            for (int i = 0; i < N; i++) {
+                Array[i] = rand() % 199 - 99;
+                SortArray[i] = Array[i];
+            }
+            cout << "Текущий неотсортированный массив:\n";
+            print_arr(Array, N);
+            cout << "\n\n";
+        }
+        else if (ind == 2) {
+            cout << "\t1. Bubble sort\n\t2. Shaker sort\n\t3. Comb sort\n\t4. Insert sort\n\t5. Quick sort\n";
+            for (int i = 0; i < N; i++) {
+                SortArray[i] = Array[i];
+            }
+            int ind2;
+            cin >> ind2;
+            if (ind2 == 1) {
+                cout << "\nНесортированный массив\n";
+                print_arr(SortArray, N);
+                auto start = steady_clock::now();
+                bubble_sort(SortArray, N);
+                auto end = steady_clock::now();
+                auto result = duration_cast<nanoseconds>(end - start);
+                cout << "\nОтсортированный массив\n";
+                print_arr(SortArray, N);
+                cout << "\nВремя сортитовки массива из 100 элеметнов сортитовкой \"bubble sort\": ";
+                cout << result.count() << " наносекунд\n\n";
+            }
+            else if (ind2 == 2) {
+                cout << "\nНесортированный массив\n";
+                print_arr(SortArray, N);
+                auto start = steady_clock::now();
+                shaker_sort(SortArray, N);
+                auto end = steady_clock::now();
+                auto result = duration_cast<nanoseconds>(end - start);
+                cout << "\nОтсортированный массив\n";
+                print_arr(SortArray, N);
+                cout << "\nВремя сортитовки массива из 100 элеметнов сортитовкой \"bubble sort\": ";
+                cout << result.count() << " наносекунд\n\n";
+            }
+            else if (ind2 == 3) {
+                cout << "\nНесортированный массив\n";
+                print_arr(SortArray, N);
+                auto start = steady_clock::now();
+                comb_sort(SortArray, N);
+                auto end = steady_clock::now();
+                auto result = duration_cast<nanoseconds>(end - start);
+                cout << "\nОтсортированный массив\n";
+                print_arr(SortArray, N);
+                cout << "\nВремя сортитовки массива из 100 элеметнов сортитовкой \"bubble sort\": ";
+                cout << result.count() << " наносекунд\n\n";
+            }
+            else if (ind2 == 4) {
+                cout << "\nНесортированный массив\n";
+                print_arr(SortArray, N);
+                auto start = steady_clock::now();
+                insert_sort(SortArray, N);
+                auto end = steady_clock::now();
+                auto result = duration_cast<nanoseconds>(end - start);
+                cout << "\nОтсортированный массив\n";
+                print_arr(SortArray, N);
+                cout << "\nВремя сортитовки массива из 100 элеметнов сортитовкой \"bubble sort\": ";
+                cout << result.count() << " наносекунд\n\n";
+            }
+            else if (ind2 == 5) {
+                cout << "\nНесортированный массив\n";
+                print_arr(SortArray, N);
+                auto start = steady_clock::now();
+                int s = 0, e = N - 1;
+                quick_sort(SortArray, e, s);
+                auto end = steady_clock::now();
+                auto result = duration_cast<nanoseconds>(end - start);
+                cout << "\nОтсортированный массив\n";
+                print_arr(SortArray, N);
+                cout << "\nВремя сортитовки массива из 100 элеметнов сортитовкой \"bubble sort\": ";
+                cout << result.count() << " наносекунд\n\n";
+            }
+        }
+        else if (ind == 3) {
+            comb_sort(SortArray, N);
+            // Поиск max, min элементов в неотсортированном массиве
+            auto start1 = steady_clock::now();
+            int min1 = Array[0], max1 = Array[0];
+            for (int i = 0; i < 100; i++) {
+                if (Array[i] < min1) {
+                    min1 = Array[i];
+                }
+                else if (Array[i] > max1) {
+                    max1 = Array[i];
+                }
+            }
+            auto end1 = steady_clock::now();
+            auto result1 = duration_cast<nanoseconds>(end1 - start1);
+
+            // Поиск max, min элементов в отсортированном массиве
+            auto start2 = steady_clock::now();
+            int min2, max2;
+            min2 = SortArray[0];
+            max2 = SortArray[N - 1];
+            auto end2 = steady_clock::now();
+            auto result2 = duration_cast<nanoseconds>(end2 - start2);
+
+            cout << "min = " << min2 << "\nmax = " << max2;
+
+            cout << "\nВремя поиска максимального и миниманого элемента\n";
+            cout << "В несортированном массиве: " << result1.count() << " наносекунд\n";
+            cout << "В отсортированном массиве: " << result2.count() << " наносекунд\n\n";
+        }
+        else if (ind == 4) {
+            comb_sort(SortArray, N);
+            int average = (SortArray[0] + SortArray[N - 1]) / 2;
+            cout << "Среднее значение: " << average << "\n";
+            cout << "Индексы: ";
+            auto start = steady_clock::now();
+            int k = 0; //количество
+            for (int i = 0; i < N; i++) {
+                if (Array[i] == average) {
+                    cout << i << " ";
+                    k += 1;
+                }
+            }
+            auto end = steady_clock::now();
+            auto result = duration_cast<nanoseconds>(end - start);
+            cout << "\nКоличество элеметнов: " << k << "\n";
+            cout << "Время: " << result.count() << " наносекунд\n\n";
+        }
+        else if (ind == 5) {
+            comb_sort(SortArray, N);
+            cout << "Введите число a: ";
+            int a, k = 0;
+            bool flag = true;
+            cin >> a;
+            for (int i = 0; i < (N * flag); i++) {
+                flag = false;
+                if (SortArray[i] < a) {
+                    k += 1;
+                    flag = true;
+                }
+            }
+            if (k > 0)
+                cout << "Количество элементов, которые меньше числа а: " << k << "\n\n";
+            else
+                cout << "Таких элементов нет\n\n";
+        }
+        else if (ind == 6) {
+            comb_sort(SortArray, N);
+            cout << "Введите число b: ";
+            int b, k = 0;
+            bool flag = true;
+            cin >> b;
+            for (int i = 0; i < (N * flag); i++) {
+                flag = false;
+                if (SortArray[N - 1 - i] > b) {
+                    k += 1;
+                    flag = true;
+                }
+            }
+            if (k > 0)
+                cout << "Количество элементов, которые больше числа b: " << k << "\n\n";
+            else
+                cout << "Таких элементов нет\n\n";
+        }
+        else if (ind == 7) {
+            comb_sort(SortArray, N);
+            cout << "Введите число: ";
+            int a;
+            cin >> a;
+            bool flag1 = false, flag2 = false;
+            //перебор (1)
+            auto start1 = steady_clock::now();
+            for (int i = 0; i < N; i++) {
+                if (SortArray[i] == a) {
+                    flag1 = true;
+                    break;
+                }
+            }
+            auto end1 = steady_clock::now();
+            auto result1 = duration_cast<nanoseconds>(end1 - start1);
+            //бинарный поиск (2)
+            auto start2 = steady_clock::now();
+            int right = N - 1, left = 0, mid;
+            while ((left <= right) && (flag2 != true)) {
+                mid = (left + right) / 2;
+                if (SortArray[mid] == a) flag2 = true;
+                if (SortArray[mid] > a) right = mid - 1;
+                else left = mid + 1;
+            }
+            auto end2 = steady_clock::now();
+            auto result2 = duration_cast<nanoseconds>(end2 - start2);
+
+            if (flag1 == true)
+                cout << "\nВведённый элемент есть в массиве\n";
+            else if (flag1 == false)
+                cout << "\nЭлемент не найден\n";
+            cout << "Время проверки полным перебором: " << result1.count() << " наносекунд\n";
+            if (flag2 == true)
+                cout << "\nВведённый элемент есть в массиве\n";
+            else if (flag2 == false)
+                cout << "\nЭлемент не найден\n";
+            cout << "Время проверки бинарным поиском: " << result2.count() << " наносекунд\n\n";
+        }
+        else if (ind == 8) {
+            int a, b;
+            cout << "Введите индексы элементов, которые вы хотите поменять местами: ";
+            cin >> a >> b;
+            if ((a < 0) || (b > 99)) {
+                cout << "\nТаких элементов нет в массиве\n\n";
+            }
+            else {
+                auto start = steady_clock::now();
+                swap(Array[a], Array[b]);
+                auto end = steady_clock::now();
+                auto result = duration_cast<nanoseconds>(end - start);
+                cout << "\nНовый массив:\n";
+                print_arr(Array, N);
+                cout << "\nВремя: " << result.count() << " наносекунд\n\n";
+            }
+        }
+        else if (ind == 0) {
+            break;
+        }
+        else {
+            cout << "Ошибка";
+        }
+    }
+}
---
+++ Практическая 3/Практическая 3/Практическая 3.cpp
@@ -0,0 +1,455 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <Windows.h>
+
+using namespace std;
+
+//Функция для перемещения по координатам консоли
+
+void XY(short x, short y, int n = 0)
+{
+    COORD p = { x, y };  //Структура, определяющая координаты в буфере консоли
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
+    this_thread::sleep_for(chrono::milliseconds(100));
+}
+
+void out(int N, int* pArr)
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (*pArr >= 0 && *pArr <= 9) {
+                cout << *(pArr++) << "     ";
+            }
+            else if (*pArr >= 10 || (*pArr <= -1 && *pArr >= -9)) {
+                cout << *(pArr++) << "    ";
+            }
+            else {
+                cout << *(pArr++) << "   ";
+            }
+
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void spiral(int N, int* pArr, short x, short y)
+{
+    int n = N--;    // n = N, N = N - 1
+    x = -4;
+    pArr--;
+    for (int i = 0; i < n; i++) {
+        pArr++;
+        *pArr = 1 + rand() % (N * N);
+        x += 6; XY(x, y);
+        cout << *pArr;
+    }
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < N; i++) {
+            pArr += n;
+            *pArr = 1 + rand() % (N * N);
+            y += 1; XY(x, y);
+            cout << *pArr;
+        }
+        for (int i = 0; i < N; i++) {
+            pArr--;
+            *pArr = 1 + rand() % (N * N);
+            x -= 6; XY(x, y);
+            cout << *pArr;
+        }
+        N--;
+        for (int i = 0; i < N; i++) {
+            pArr -= n;
+            *pArr = 1 + rand() % (N * N);
+            y -= 1; XY(x, y);
+            cout << *pArr;
+        }
+        for (int i = 0; i < N; i++) {
+            pArr++;
+            *pArr = 1 + rand() % (N * N);
+            x += 6; XY(x, y);
+            cout << *pArr;
+        }
+        N--;
+    }
+    y += (n / 2 + 1); x = 0; XY(x, y);
+}
+
+void snake(const int N, int* pArr, short x, short y)
+{
+    x = 1;
+    *pArr = 1 + rand() % (N * N);
+    XY(x, y); cout << *pArr;
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = 1; j < N; j++) {
+            pArr += N;
+            *pArr = 1 + rand() % (N * N);
+            y += 1; XY(x, y);
+            cout << *pArr;
+        }
+        pArr++; *pArr = 1 + rand() % (N * N);
+        x += 6; XY(x, y);
+        cout << *pArr;
+        for (int j = 1; j < N; j++) {
+            pArr -= N;
+            *pArr = 1 + rand() % (N * N);
+            y -= 1; XY(x, y);
+            cout << *pArr;
+        }
+        pArr++;
+        if (i != N / 2 - 1) {
+            *pArr = 1 + rand() % (N * N);
+            x += 6; XY(x, y);
+            cout << *pArr;
+        }
+    }
+    y += N + 1; x = 0; XY(x, y);
+}
+
+void transpositionA(const int N, int* pArr) //Меняет подматрицы по часовой стрелке
+{
+    int* p1Arr = pArr, * p2Arr = pArr + N / 2, * p3Arr = pArr, * p4Arr = pArr + N * N / 2, * p5Arr = pArr + N * N / 2, * p6Arr = p4Arr + N / 2;
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            swap(*(p1Arr++), *(p2Arr++));
+        }
+        p1Arr += N / 2, p2Arr += N / 2;
+    }
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            swap(*(p3Arr++), *(p4Arr++));
+        }
+        p3Arr += N / 2, p4Arr += N / 2;
+    }
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            swap(*(p5Arr++), *(p6Arr++));
+        }
+        p5Arr += N / 2, p6Arr += N / 2;
+    }
+    cout << "Новый массив:\n"; out(N, pArr);
+}
+
+void transpositionB(const int N, int* pArr) //Меняет диагональные подматрицы
+{
+    int* p1Arr = pArr, * p3Arr = pArr + N / 2, * p4Arr = pArr + N * N / 2, * p2Arr = p4Arr + N / 2;
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            swap(*(p1Arr++), *(p2Arr++));
+        }
+        p1Arr += N / 2, p2Arr += N / 2;
+    }
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            swap(*(p3Arr++), *(p4Arr++));
+        }
+        p3Arr += N / 2, p4Arr += N / 2;
+    }
+    cout << "Новый массив:\n"; out(N, pArr);
+}
+
+void transpositionC(const int N, int* pArr)  //Меняет верхние и нижние подматрицы
+{
+    int* p1Arr = pArr, * p2Arr = pArr + N * N / 2;
+    for (int i = 0; i < N * N / 2; i++) {
+        swap(*(p1Arr++), *(p2Arr++));
+    }
+    cout << "Новый массив:\n"; out(N, pArr);
+}
+
+void transpositionD(const int N, int* pArr) //Меняет правые и левые подматрицы
+{
+    int* p1Arr = pArr, * p2Arr = pArr + N / 2;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            swap(*(p1Arr++), *(p2Arr++));
+        }
+        p1Arr += N / 2, p2Arr += N / 2;
+    }
+    cout << "Новый массив:\n"; out(N, pArr);
+}
+
+void _plus(int N, int* pArr, int value) {
+    for (int i = 0; i < N * N; i++) {
+        *(pArr + i) += value;
+    }
+    cout << "\n"; out(N, pArr);
+}
+
+void _minus(int N, int* pArr, int value) {
+    for (int i = 0; i < N * N; i++) {
+        *(pArr + i) -= value;
+    }
+    cout << "\n"; out(N, pArr);
+}
+
+void _multiply(int N, int* pArr, int value) {
+    for (int i = 0; i < N * N; i++) {
+        *(pArr + i) *= value;
+    }
+    cout << "\n"; out(N, pArr);
+}
+
+void _divide(int N, int* pArr, int value) {
+    for (int i = 0; i < N * N; i++) {
+        *(pArr + i) /= value;
+    }
+    cout << "\n"; out(N, pArr);
+}
+
+void sort(const int N, int* pArr)
+{
+    int t;
+    for (int i = 0; i < N * N; i++) {
+        for (int j = i + 1; j < N * N; j++) {
+            if (*(pArr + j) < *(pArr + i)) {
+                swap(*(pArr + j), *(pArr + i));
+            }
+        }
+    }
+    cout << "Отсортированный массив:\n"; out(N, pArr);
+}
+
+void individualtask10()
+{
+    const int N = 3;
+    int Arr[N][N];
+    int* pArr = &Arr[0][0];
+    int det;
+    for (int i = 0; i < N * N; i++) {
+        *pArr = -30 + rand() % 61;
+        pArr++;
+    }
+    pArr -= N * N;
+    cout << "\nМатрица 3x3: \n";
+    out(N, pArr);
+    det = Arr[0][0] * Arr[1][1] * Arr[2][2] + Arr[0][1] * Arr[1][2] * Arr[2][0]
+        + Arr[1][0] * Arr[2][1] * Arr[0][2] - Arr[2][0] * Arr[1][1] * Arr[0][2] -
+        Arr[2][1] * Arr[1][2] * Arr[0][0] - Arr[1][0] * Arr[0][1] * Arr[2][2];
+    cout << "Подсчитаем определитель\n";
+    cout << "(" << Arr[0][0] << " * " << Arr[1][1] << " * " << Arr[2][2] << ")" << " + " << "(" << Arr[0][1] << " * " << Arr[1][2] << " * " << Arr[2][0] << ")"
+        " + " << "(" << Arr[1][0] << " * " << Arr[2][1] << " * " << Arr[0][2] << ")" << " - " << "(" << Arr[2][0] << " * " << Arr[1][1] << " * " << Arr[0][2] << ")" << " - " << "(" <<
+        Arr[2][1] << " * " << Arr[1][2] << " * " << Arr[0][0] << ")" << " - " << "(" << Arr[1][0] << " * " << Arr[0][1] << " * " << Arr[2][2] << ")" << " = " << det;
+    cout << "\n\nОпределитель равен: " << det << "\n\n\n";
+}
+
+void individualtask5()
+{
+    const int N = 6;
+    int Arr[N][N];
+    int* pArr = &Arr[0][0];
+    cout << "\nМатрица, заполненная случайными числами от 1 до N*N:\n";
+    for (int i = 0; i < N * N; i++) {
+        *pArr = 1 + rand() % (N * N);
+        pArr++;
+    }
+    pArr -= N * N;
+    out(N, pArr);
+
+    int Arr2[N][N];
+    int* pArr2 = &Arr2[0][0];
+    cout << "Вторая матрица, заполненная случайными числами от 1 до N*N:\n";
+    for (int i = 0; i < N * N; i++) {
+        *pArr2 = 1 + rand() % (N * N);
+        pArr2++;
+    }
+    pArr2 -= N * N;
+    out(N, pArr2);
+
+    //ввчесть из второй матрицы первую
+    for (int i = 0; i < N * N; i++) {
+        *(pArr++) -= *(pArr2++);
+    }
+    pArr -= N * N;
+    cout << "Вычтем из второй сгенерированной матрицы первую\n";
+    cout << "Теперь матрица имеет вид: \n";
+    out(N, pArr);
+}
+
+int main()
+{
+    setlocale(0, "Rus");
+    srand(time(0));
+
+    bool flag = true;
+
+    while (flag) {
+
+        short x = 0, y = 0;
+        int Arr[100];
+        int* pArr = Arr;
+        int N = 1;
+
+        cout << "Введите размерность квадратной матрицы 6, 8, 10: ";
+
+        y += 1;
+
+        cin >> N;
+
+        while (N != 6 && N != 8 && N != 10)
+        {
+            cout << "\nНе корректно. Введите размерность матрицы заново: ";
+            cin >> N;
+            y += 2;
+        }
+
+        cout << "\nВыбирите вариант заполнения матрицы:\n"
+            "1. Заполнение матрицы по спирали\n"
+            "2. Заполнение матрицы по столбцам\n";
+
+        y += 4;
+
+        int operation = 0;
+        cin >> operation;
+
+        while (operation != 1 && operation != 2)
+        {
+            cout << "\nНе корректно. Введите вариант заполнения заново: ";
+            cin >> operation;
+            y += 2;
+        }
+
+        y += 2;
+
+        switch (operation)
+        {
+        case 1:
+        {
+            spiral(N, pArr, x, y);
+            break;
+        }
+        case 2:
+        {
+            snake(N, pArr, x, y);
+            break;
+        }
+        default:
+            break;
+        }
+
+        cout <<
+            "\n\tВыберите действие:\n"
+            "0. Завершение\n"
+            "1. Перестановка блоков (a)\n"
+            "2. Перестановка блоков (b)\n"
+            "3. Перестановка блоков (c)\n"
+            "4. Перестановка блоков (d)\n"
+            "5. Сортировка матрицы\n"
+            "6. Прибавить к элементам матрицы число (+)\n"
+            "7. Вычесть из элементов матрицы число (-)\n"
+            "8. Умножить элементы матрицы на число (*)\n"
+            "9. Разделите элементы матрицы на число (/)\n"
+            "10. Задать новый массив\n"
+            "11. Индивидуальное задание(10)\n"
+            "12. Индивидуальное задание(5)\n\n";
+        cout << "Введите номер выбранного варианта: ";
+
+        bool flag_ = true;
+
+        while (flag_) {
+
+            int option = 111;
+            cin >> option;
+
+            switch (option)
+            {
+            case 1:
+            {
+                transpositionA(N, pArr);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 2:
+            {
+                transpositionB(N, pArr);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 3:
+            {
+                transpositionC(N, pArr);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 4:
+            {
+                transpositionD(N, pArr);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 5:
+            {
+                sort(N, pArr);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 6:
+            {
+                int value;
+                cout << "(+) Введите число: ";
+                cin >> value;
+                _plus(N, pArr, value);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 7:
+            {
+                int value;
+                cout << "(-) Введите число: ";
+                cin >> value;
+                _minus(N, pArr, value);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 8:
+            {
+                int value;
+                cout << "(*) Введите число: ";
+                cin >> value;
+                _multiply(N, pArr, value);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 9:
+            {
+                int value;
+                cout << "(/) Введите число: ";
+                cin >> value;
+                _divide(N, pArr, value);
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 10:
+            {
+                flag_ = false;
+                system("cls");
+                break;
+            }
+            case 11:
+            {
+                //Individual task 10
+                individualtask10();
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 12:
+            {
+                //Individual task 5
+                individualtask5();
+                cout << "\nДля прололжения введите действие: ";
+                break;
+            }
+            case 0:
+            {
+                flag_ = false;
+                flag = false;
+                break;
+            }
+            default:
+                cout << "\nНе корректно. Введите действие заново: ";
+                break;
+            }
+        }
+    }
+}
